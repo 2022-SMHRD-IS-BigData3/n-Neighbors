20c09766d0a8d1b8095478d2d1dc8f616ce78ce4
이상원 <skdld570@gmail.com>
2023-07-21 15:04:31 +0900

    flask와 연결했습니다! 잘 필터링되네요!

--- Practice-1/src/main/java/com/smhrd/config/WebSocketConfig.java
+++ Practice-1/src/main/java/com/smhrd/config/WebSocketConfig.java
@@ -1,14 +1,12 @@
 package com.smhrd.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
-
-import com.smhrd.controller.UserNickHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -27,22 +25,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		
 		registry.setApplicationDestinationPrefixes("/pub");
 	}
+    
+    // flask bean
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
+    }
+    
 }   
-//    private HandshakeInterceptor getHandshakeInterceptor() {
-//        return new UserNickHandshakeInterceptor();
-//    }
 
-//@Configuration
-//@EnableWebSocketMessageBroker // WebSocket 메시지 처리 활성화
-//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/topic");
-//        registry.setApplicationDestinationPrefixes("/app");
-//    }
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/chat").withSockJS();
-//    }
 
